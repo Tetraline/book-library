ae8263760941bb41c9d95fb6c9df34dc484b1dc4
Tetraline <danielsamways@gmail.com>
2022-10-05 13:07:36 +0100

    user login and permissions

--- src/main/java/org/daniel/Library.java
+++ src/main/java/org/daniel/Library.java
@@ -5,11 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Comparator;
+import java.util.Objects;
 
 public class Library {
-    private String bookFilePath;
-    private String userFilePath;
+    private final String bookFilePath;
+    private final String userFilePath;
 
     public Library(String bookFilePath, String userFilePath) {
         this.bookFilePath = bookFilePath;
@@ -19,39 +20,104 @@ public class Library {
     public void runTerminal() throws IOException {
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
+        int userId = -1;
+        String userName = "Not Logged In";
+
+        //noinspection InfiniteLoopStatement
         while (true) {
-            System.out.print(">");
+            System.out.print(userName + ">");
             String[] input = reader.readLine().split(" ");
             switch (input[0]) {
                 case "new":
+                    if (userId != 0) {
+                        System.out.println("Insufficient Permission");
+                        break;
+                    }
                     if (input[1].equals("user")) {
                         System.out.println(newUser(input[2]));
                     }
                     break;
                 case "loan":
-                    System.out.println(loan(Integer.parseInt(input[1]), Integer.parseInt(input[3])));
+                    if (Integer.parseInt(input[3]) == userId || userId == 0) {
+                        System.out.println(loan(Integer.parseInt(input[1]), Integer.parseInt(input[3])));
+                        break;
+                    }
+                    System.out.println("Insufficient Permission");
                     break;
                 case "return":
                     System.out.println(bookReturn(Integer.parseInt(input[1])));
                     break;
                 case "report":
-                    report(input[1]);
+                    switch (input[1]) {
+                        case "user":
+                            userReport(input[2]);
+                            break;
+                        case "timesLoaned":
+                            timesLoaned();
+                            break;
+                        case "loanedBooks":
+                            loanedBooks();
+                            break;
+                    }
+                    break;
+                case "login":
+                    userId = Integer.parseInt(input[1]);
+                    if (userId == 0) {
+                        userName = "admin";
+                    } else {
+                        User[] users = FileUtility.readUsers(userFilePath);
+                        int finalUserId = userId;
+                        User user = Arrays.stream(users).filter(u -> u.getId() == finalUserId).findFirst().orElse(null);
+                        try {
+                            userName = Objects.requireNonNull(user).getName();
+                        } catch (Exception e) {
+                            System.out.println("User could not be found");
+                        }
+                    }
                     break;
             }
         }
 
     }
 
-    public String newUser(String name) {
+    private void loanedBooks() {
+        Book[] books = FileUtility.readBooks(bookFilePath);
+        Book[] loaned = Arrays.stream(books).filter(b -> b.getLocation() != 0).toArray(Book[]::new);
+        if (loaned.length == 0) {
+            System.out.println("No books are loaned");
+        }
+        for (Book b : loaned) {
+            System.out.println(b.getId() + ":" + b.getTitle() + " is loaned to " + b.getLocation());
+        }
+    }
+
+    private void timesLoaned() {
+        Book[] books = FileUtility.readBooks(bookFilePath);
+        books = Arrays.stream(books).sorted(Comparator.comparing(Book::getTimesLoaned).reversed()).toArray(Book[]::new);
+        for (Book b : books) {
+            System.out.println(b.getTimesLoaned() + "  |  " + b.getTitle());
+        }
+    }
+
+    private void userReport(String userId) {
+        Book[] books = FileUtility.readBooks(bookFilePath);
+        books = Arrays.stream(books).filter(b -> b.getLocation() == Integer.parseInt(userId)).toArray(Book[]::new);
+        for (Book b : books) {
+            System.out.println(b.getTitle());
+        }
+
+    }
+
+    private String newUser(String name) {
         User[] users = FileUtility.readUsers(userFilePath);
         ArrayList<User> userList = new ArrayList<>(Arrays.asList(users));
-        int newId = 1 + userList.stream().mapToInt(u -> u.getId()).max().orElse(0);
+        int newId = 1 + userList.stream().mapToInt(User::getId).max().orElse(0);
         userList.add(new User(newId, name));
         FileUtility.write(userList.toArray(), userFilePath);
         return String.format("`%s` has joined the library", name);
     }
 
-    public String loan(int bookId, int userId) {
+    private String loan(int bookId, int userId) {
         Book[] books = FileUtility.readBooks(bookFilePath);
         Book book = Arrays.stream(books).filter(b -> b.getId() == bookId).findFirst().orElse(null);
         User[] users = FileUtility.readUsers(userFilePath);
@@ -63,7 +129,7 @@ public class Library {
         if (user == null) {
             return "That user doesn't exist!";
         }
-        if (book.getLocation() != 0){
+        if (book.getLocation() != 0) {
             return "That book is already out on loan!";
         }
         book.incrementTimesLoaned();
@@ -73,19 +139,14 @@ public class Library {
     }
 
 
-    public String bookReturn(int bookId) {
+    private String bookReturn(int bookId) {
         Book[] books = FileUtility.readBooks(bookFilePath);
         Book book = Arrays.stream(books).filter(b -> b.getId() == bookId).findFirst().orElse(null);
-        if(book.getLocation()==0){
+        if (Objects.requireNonNull(book).getLocation() == 0) {
             return "That book is already at the library";
         }
         book.setLocation(0);
         FileUtility.write(books, bookFilePath);
-        return String.format("`%s` successfully returned",book.getTitle());
+        return String.format("`%s` successfully returned", book.getTitle());
     }
-
-    public void report(String type) {
-
-    }
-
 }
